50f34175df22a7cb5d930c317f3e866f9f66685d
Hrics12 <111898293+Hrics12@users.noreply.github.com>
2024-05-02 11:32:12 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,21 @@
 are_isomorphic(graph1, graph2) {
-    return undefined;
+    if (graph1.vertices.length !== graph2.vertices.length || graph1.edges.length !== graph2.edges.length) {
+    return false;
+}
+
+for (let vertex of graph1.vertices) {
+    let degree1 = graph1.degree(vertex);
+    let found = false;
+    for (let vertex2 of graph2.vertices) {
+        if (graph2.degree(vertex2) === degree1) {
+            found = true;
+            break;
+        }
+    }
+    if (!found) {
+        return false;
+    }
+}
+return true;
+  
 }
